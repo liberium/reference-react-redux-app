fc5d1a2326a4d081e7138d107c14c1a67a7badd0
Sergey Filkin <sergey.filkin@gmail.com>
2018-10-23 22:23:45 +0300

    switch to redux-thunk-actions

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,9 +1,11 @@
-import {createActions} from 'redux-action'
+import {createActionThunk} from 'redux-thunk-actions'
+import api from '@src/api'
 
-const {login, logout} = createActions({
-  LOGIN: credentials => ({credentials}),
-  REGISTER,
-  RESET_PASSWORD,
-  CHANGE_CREDENTIALS,
-  LOGOUT
-})
+export const login = createActionThunk('LOGIN', credentials => api.login(credentials))
+export const logout = createActionThunk('LOGOUT', api.logout)
+export const register = createActionThunk('REGISTER', user => api.register(user))
+export const resetPassword = createActionThunk('RESET_PASSWORD', api.resetPassword)
+export const changeCredentials = createActionThunk(
+  'CHANGE_CREDENTIALS',
+  newCredentials => api.changeCredentials(newCredentials)
+)
