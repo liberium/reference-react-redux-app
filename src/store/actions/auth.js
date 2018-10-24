97afa531ee2f9c97b32df3eb7de80ed646fc4672
Sergey Filkin <sergey.filkin@gmail.com>
2018-10-24 15:46:23 +0300

    prettify the codebase

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,11 +1,10 @@
-import {createActionThunk} from 'redux-thunk-actions'
+import { createActionThunk } from 'redux-thunk-actions'
 import api from '@src/api'
 
 export const login = createActionThunk('LOGIN', credentials => api.login(credentials))
 export const logout = createActionThunk('LOGOUT', api.logout)
 export const register = createActionThunk('REGISTER', user => api.register(user))
 export const resetPassword = createActionThunk('RESET_PASSWORD', api.resetPassword)
-export const changeCredentials = createActionThunk(
-  'CHANGE_CREDENTIALS',
-  newCredentials => api.changeCredentials(newCredentials)
+export const changeCredentials = createActionThunk('CHANGE_CREDENTIALS', newCredentials =>
+  api.changeCredentials(newCredentials)
 )
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,5 +1,5 @@
-import {handleActions, combineActions } from 'redux-actions'
-import {login, logout, register, resetPassword, changeCredentials} from '@src/store/actions'
+import { handleActions, combineActions } from 'redux-actions'
+import { login, logout, register, resetPassword, changeCredentials } from '@src/store/actions'
 
 const initialState = {
   userName: '',
@@ -14,10 +14,12 @@ const authReducer = handleActions({
     register.START,
     resetPassword.START,
     changeCredentials.START
-  )]: state => ({...state, loading: true}),
-  [login.SUCCEEDED]:
-  (state, {payload: {name, email}}) =>
-  ({...state, userName: name, userEmail: email})
+  )]: state => ({ ...state, loading: true }),
+  [login.SUCCEEDED]: (state, { payload: { name, email } }) => ({
+    ...state,
+    userName: name,
+    userEmail: email
+  })
 })
 
 export default authReducer
