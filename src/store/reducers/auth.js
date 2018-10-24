3dac9ffbcf9862b25fbd291ab9ac30b8ffb8be51
Sergey Filkin <sergey.filkin@gmail.com>
2018-10-24 15:18:02 +0300

    change auth reducer

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,5 +1,23 @@
 import {handleActions, combineActions } from 'redux-actions'
+import {login, logout, register, resetPassword, changeCredentials} from '@src/store/actions'
 
-const authReducer = handleActions({})
+const initialState = {
+  userName: '',
+  userEmail: '',
+  loading: false
+}
+
+const authReducer = handleActions({
+  [combineActions(
+    login.START,
+    logout.START,
+    register.START,
+    resetPassword.START,
+    changeCredentials.START
+  )]: state => ({...state, loading: true}),
+  [login.SUCCEEDED]:
+  (state, {payload: {name, email}}) =>
+  ({...state, userName: name, userEmail: email})
+})
 
 export default authReducer
