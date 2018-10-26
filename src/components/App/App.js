f283ef99f7a0cb909894561d55df3c5c4a8d9060
Sergey Filkin <sergey.filkin@gmail.com>
2018-10-26 22:19:13 +0300

    add react-router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { LoginScreen } from '@src/containers'
 
 function App() {
-  return null
+  return (
+    <Provider store={store}>
+      <Router>
+        <Route exact path="/" component={LoginScreen} />
+      </Router>
+    </Provider>
+  )
 }
 
 export default App
---
+++ src/containers/LoginScreen/LoginScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+class LoginScreen extends React.Component {
+  render() {
+    return null
+  }
+}
+
+export default connect()(LoginScreen)
