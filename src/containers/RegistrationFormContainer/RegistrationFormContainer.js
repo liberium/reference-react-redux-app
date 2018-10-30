f45ae2e3a07c0d6de119152c89df2a0404d2a1db
Sergey Filkin <sergey.filkin@gmail.com>
2018-10-30 08:58:48 +0300

    add registration screen

---
+++ src/containers/RegistrationFormContainer/RegistrationFormContainer.js
@@ -0,0 +1,4 @@
+import { connect } from 'react-redux'
+import { RegistrationForm } from '@src/components'
+
+export default connect()(RegistrationForm)
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,1 +1,3 @@
 export { default as LoginScreen } from './LoginScreen'
+export { default as RegistrationScreen } from './RegistrationScreen'
+export { default as RegistrationFormContainer } from './RegistrationFormContainer'
