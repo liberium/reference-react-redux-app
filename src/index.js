2c3ddfceaf6144c2fc5fd6dbf231842e6be316cc
Sergey Filkin <sergey.filkin@gmail.com>
2018-11-22 17:07:31 +0300

    add main module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { App } from 'components'
-
-ReactDOM.render(<App />, document.getElementById('root'))
+export { default } from './main'
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { App } from 'components'
+
+const appRef = ReactDOM.render(<App />, document.getElementById('reference-react-redux-app-root'))
+
+export default appRef
